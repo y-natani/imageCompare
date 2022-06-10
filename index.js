c5eef5020a8a44ee98afa6446b6feada9acca6d6
y-natani <y-natani@yukoyuko.co.jp>
2022-06-11 00:57:05 +0900

    try catch

--- index.js
+++ index.js
@@ -3,24 +3,36 @@ const puppeteer = require('puppeteer');
 const { actualDomain, expectDomain, files } = require('./env');
 const { makeFileName, delay, autoScroll } = require('./helpers');
 
-
+const errorPages = []
 
 const takeScreenshot = async (url, imageFileName) => {
   console.log(`taking screenshot... ${url}`)
-  const browser = await puppeteer.launch({headless: true});
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1920, height: 1080 });
-  await page.waitForTimeout(10000);
-  await page.goto(url, { waitUntil: ['load'] });
-  await autoScroll(page);
-  await page.screenshot({path: imageFileName, fullPage:true});
-  await browser.close()
+  let browser, page;
+  try{
+    browser = await puppeteer.launch({headless: true});
+    page = await browser.newPage();
+    await page.setViewport({ width: 1920, height: 1080 });
+    await page.waitForTimeout(10000);
+    await page.goto(url, { waitUntil: ['networkidle2'] });
+    await autoScroll(page);
+    await page.screenshot({path: imageFileName, fullPage:true});
+  }catch{
+    console.log("Error in", url)
+    errorPages.push(url)
+  }finally{
+    if(browser){
+      await browser.close()
+    }
+  }
 }
 
 (async () => {
   console.log(`開始時刻`, new Date().toString())
   for(const url of files){
     console.log(`進捗: ${files.indexOf(url)} / ${files.length}`)
+    if(errorPages.length){
+      console.log('errorPage', errorPages)
+    }
     await takeScreenshot(`${actualDomain.domain}/${url}`,`actual/${makeFileName(url)}`);
     await takeScreenshot(`${expectDomain.domain}/${url}`, `expected/${makeFileName(url)}`);
   }
