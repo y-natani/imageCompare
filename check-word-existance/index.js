2cce6a866e5052143f480bfc8de41b038a0644b5
y-natani <y-natani@yukoyuko.co.jp>
2022-07-06 14:52:40 +0900

    fix

--- check-word-existance/index.js
+++ check-word-existance/index.js
@@ -11,16 +11,18 @@ targetWords.forEach((word) => {
   let found = false;
   targetDirectories.forEach((directory) => {
     if (found) return;
-    const stdout = execSync(
-      `find ${directory} -type f -print | xargs grep '${word}'`
-    );
-
-    found = stdout.toString().split(word).length > 2;
-
-    if (found && OPTIONS.showIfFound) {
-      console.log(`FOUND ${word} in ${directory}`);
-    } else if (!found && OPTIONS.showIfNotFound) {
-      console.log(`NOT FOUND: ${word} in ${directory}`);
+    try {
+      const stdout = execSync(
+        `find ${directory} -type f -print | xargs grep '${word}'`
+      );
+      found = stdout.toString().split(word).length > 2;
+      if (found && OPTIONS.showIfFound) {
+        console.log(`FOUND ${word} in ${directory}`);
+      }
+    } catch (e) {
+      if (OPTIONS.showIfNotFound) {
+        console.log(`NOT FOUND: ${word} in ${directory}`);
+      }
     }
   });
 });
